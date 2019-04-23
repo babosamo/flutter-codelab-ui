a138f65492425b2b96ef61cb8f7f4d290903be7d
noah <noah.kim@kakaocorp.com>
2019-04-24 00:08:03 +0900

    7#

--- codelab_ui/lib/main.dart
+++ codelab_ui/lib/main.dart
@@ -17,24 +17,33 @@ class ChatScreen extends StatefulWidget {
   State createState() => new ChatScreenState();
 }
 
-class ChatScreenState extends State<ChatScreen> {
+class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
   final List<ChatMessage> _messages = <ChatMessage>[]; // new
   final TextEditingController _textController = new TextEditingController();
 
+  @override
+  void dispose() {                                                   //new
+    for (ChatMessage message in _messages)                           //new
+      message.animationController.dispose();                         //new
+    super.dispose();                                                 //new
+  }                                                                  //new
+
   Widget _buildTextComposer() {
     return new IconTheme(
-        data: new IconThemeData(color: Theme.of(context).accentColor),
+        data: new IconThemeData(color: Theme
+            .of(context)
+            .accentColor),
         child: new Container(
             margin: const EdgeInsets.symmetric(horizontal: 5.0),
             child: new Row(
               children: <Widget>[
                 new Flexible(
                     child: new TextField(
-                  controller: _textController,
-                  onSubmitted: _handleSubmitted,
-                  decoration:
+                      controller: _textController,
+                      onSubmitted: _handleSubmitted,
+                      decoration:
                       new InputDecoration.collapsed(hintText: "Send a Message"),
-                )),
+                    )),
                 new Container(
                   margin: new EdgeInsets.symmetric(horizontal: 4.0),
                   child: new IconButton(
@@ -48,13 +57,16 @@ class ChatScreenState extends State<ChatScreen> {
   void _handleSubmitted(String text) {
     _textController.clear();
     ChatMessage message = new ChatMessage(
-      //new
       text: text, //new
+      animationController: new AnimationController(
+          duration: new Duration(microseconds: 700),
+          vsync: this),
     ); //new
     setState(() {
       //new
       _messages.insert(0, message); //new
     });
+    message.animationController.forward();
   }
 
   @override
@@ -73,7 +85,9 @@ class ChatScreenState extends State<ChatScreen> {
           new Divider(height: 1.0),
           new Container(
             padding: new EdgeInsets.all(5.0),
-            decoration: new BoxDecoration(color: Theme.of(context).cardColor),
+            decoration: new BoxDecoration(color: Theme
+                .of(context)
+                .cardColor),
             child: _buildTextComposer(),
           ),
         ],
@@ -85,32 +99,41 @@ class ChatScreenState extends State<ChatScreen> {
 const String _name = "Noah";
 
 class ChatMessage extends StatelessWidget {
-  ChatMessage({this.text});
-
+  ChatMessage({this.text, this.animationController}); //modified
   final String text;
+  final AnimationController animationController;
 
   @override
   Widget build(BuildContext context) {
-    return new Container(
-      margin: const EdgeInsets.symmetric(vertical: 10.0),
-      child: new Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          new Container(
-            margin: const EdgeInsets.only(right: 16.0),
-            child: new CircleAvatar(child: new Text(_name[0])),
-          ),
-          new Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              new Text(_name, style: Theme.of(context).textTheme.subhead),
-              new Container(
-                margin: const EdgeInsets.only(top: 5.0),
-                child: new Text(text),
-              )
-            ],
-          )
-        ],
+    return new SizeTransition
+      (
+      sizeFactor: new CurvedAnimation(
+          parent: animationController, curve: Curves.easeOut),
+      axisAlignment: 0.0,
+      child: new Container(
+        margin: const EdgeInsets.symmetric(vertical: 10.0),
+        child: new Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new Container(
+              margin: const EdgeInsets.only(right: 16.0),
+              child: new CircleAvatar(child: new Text(_name[0])),
+            ),
+            new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                new Text(_name, style: Theme
+                    .of(context)
+                    .textTheme
+                    .subhead),
+                new Container(
+                  margin: const EdgeInsets.only(top: 5.0),
+                  child: new Text(text),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
